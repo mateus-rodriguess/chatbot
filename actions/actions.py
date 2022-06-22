2c0d04d602e1fd3b56103d26fa130bd377ad238a
Mateus Rodrigues <mateus18nc@gmail.com>
2022-06-22 15:08:25 -0300

    change: request api connect

--- actions/actions.py
+++ actions/actions.py
@@ -12,16 +12,20 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from actions.pln_request import request_pln
+
+
 class ActionHelloWorld(Action):
 
     def name(self) -> Text:
-       return "action_feed_back"
+        return "action_feed_back"
 
-    def run(self, dispatcher: CollectingDispatcher,tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         data_message = {}
         message = tracker.latest_message['text']
-        
+
         data_message["message"] = message
+        data_message["save"] = True
+        data_message["translate"] = True
         data_message["user"] = "whatsapp"
         request_pln(data_message)
         dispatcher.utter_message(text="Obrigado pelo Feedback")
--- actions/pln_request.py
+++ actions/pln_request.py
@@ -4,9 +4,8 @@ import requests
 
 def request_pln(data: dict):
 
-    url = "https://c21f-186-225-176-38.sa.ngrok.io/accuracy"
+    url = "https://6970-177-128-80-105.sa.ngrok.io/accuracy"
     try:
         requests.post(url=url, json=data)
-    except Exception as erro:
-        print("--------------------------------")
-        print(erro)
+    except Exception as error:
+        pass
